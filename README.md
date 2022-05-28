6ab7c5ecd202f32a811ea3ca95251e757f102a71
Hundraw <chauvel.dimitri@posteo.net>
2022-05-28 23:07:59 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# GrapeFruit
+> It's my own template for create a new web developement project
+
+### Install packages
+```sh
+npm install
+```
---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+"use strict";
+// -----  IMPORTS  ----- //
+
+// ----- VARIABLES ----- //
+
+// ----- EXECUTION ----- //
+document.addEventListener('DOMContentLoaded', () => {
+
+    //code
+
+});
